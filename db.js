12a6d33d9d872b9b499f7c8c07e28d9dc64f0e05
socialcrashout <phantomreaper875@gmail.com>
2026-08-22 08:48:19 -0500

    fff

--- db.js
+++ db.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const { MongoClient } = require('mongodb');
+const mongoose = require('mongoose');
 
 const uri = process.env.MONGO_URI;
 const client = new MongoClient(uri);
@@ -9,8 +10,15 @@ let db;
 async function connectDB() {
     if (db) return db; // already connected
     await client.connect();
-    db = client.db("lorenzo"); // pick any db name you like
+    db = client.db("lorenzo");
     console.log("Connected to MongoDB");
+
+    // Also connect mongoose, since some modules (e.g. infractionManager) use it
+    if (mongoose.connection.readyState === 0) {
+        await mongoose.connect(uri);
+        console.log("Mongoose connected");
+    }
+
     return db;
 }
 
